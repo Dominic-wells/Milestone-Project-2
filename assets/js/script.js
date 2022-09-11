4fca9f65ccc9854432c10f2944152dd3cb391e77
Dominic-wells <joedomwells@gmail.com>
2022-09-11 09:59:21 +0100

    api look up and user input(nonworking)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
-const canvas = document.querySelector("canvas");
+"use strict";
 
-console.log(canvas);
+const len = document.getElementById("passlen");
+const api = "https://password-generator1.p.rapidapi.com/api/generePassWd?len=";
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "0886325422msha71678830f94f69p1092c7jsnb44591722c0d",
+    "X-RapidAPI-Host": "password-generator1.p.rapidapi.com",
+  },
+};
+
+function generate() {
+  const generate = fetch(api, len, options)
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
+}
