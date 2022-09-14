9f5e82830c84a3a0ab9ebf1e7f4e9cfa436305fd
Dominic-wells <joedomwells@gmail.com>
2022-09-15 00:51:55 +0100

    Will display api results to html from user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,18 +3,14 @@ document.querySelector("#genratorPass").addEventListener("click", () => {
   request(user_input);
 });
 
-const passworldEl = document.getElementById("dispPassword");
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "0886325422msha71678830f94f69p1092c7jsnb44591722c0d",
-    "X-RapidAPI-Host": "password-generator1.p.rapidapi.com",
-  },
-};
+// const passworldEl = document.getElementById("dispPassword");
 
 request = (user_input) => {
-  let url = `https://password-generator1.p.rapidapi.com/api/generePassWd?len=${user_input.value}`;
-  fetch(url, options)
+  let url = `https://passwordinator.herokuapp.com?len=${user_input.value}`;
+  fetch(url)
     .then((response) => response.json())
-    .then((responseJson) => console.log(responseJson));
+    .then((data) => {
+      console.log(data.data);
+      document.querySelector("#dispPassword").innerHTML = data.data;
+    });
 };
