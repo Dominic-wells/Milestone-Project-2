3e227eab9bd36276ec37f6861c9bc41ddc233f39
Dominic-wells <joedomwells@gmail.com>
2022-09-17 00:13:04 +0100

    Changed API and added Num prams

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,21 @@
 document.querySelector("#genratorPass").addEventListener("click", () => {
-  const user_input = document.querySelector("#passlen");
-  request(user_input);
+  const user_input_text = document.querySelector("#passlen");
+  const user_input_number = document.querySelector(
+    "input[name=number]:checked"
+  )?.value;
+
+  request(user_input_text, user_input_number);
 });
+const passworldEl = document.getElementById("dispPassword");
 
-// const passworldEl = document.getElementById("dispPassword");
+document.querySelector('input[name="number]:checked');
 
-request = (user_input) => {
-  let url = `https://passwordinator.herokuapp.com?len=${user_input.value}`;
+request = (user_input_text, user_input_number) => {
+  let url = `https://www.psswrd.net/api/v1/password/?length=${user_input_text.value}&int=${user_input_number}`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data.data);
-      document.querySelector("#dispPassword").innerHTML = data.data;
+      console.log(data);
+      document.querySelector("#dispPassword").innerHTML = data.password;
     });
 };
