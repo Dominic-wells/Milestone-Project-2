6266eeabcbd220954d39cae71f18d82a00769f46
Dominic-wells <joedomwells@gmail.com>
2022-09-17 02:17:36 +0100

    Changed API due outside being Ip locked

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,19 +3,38 @@ document.querySelector("#genratorPass").addEventListener("click", () => {
   const user_input_number = document.querySelector(
     "input[name=number]:checked"
   )?.value;
-
-  request(user_input_text, user_input_number);
+  const user_input_upper = document.querySelector(
+    "input[name=upper]:checked"
+  )?.value;
+  const user_input_special = document.querySelector(
+    "input[name=spec]:checked"
+  )?.value;
+  request(
+    user_input_text,
+    user_input_number,
+    user_input_upper,
+    user_input_special
+  );
 });
 const passworldEl = document.getElementById("dispPassword");
 
 document.querySelector('input[name="number]:checked');
 
-request = (user_input_text, user_input_number) => {
-  let url = `https://www.psswrd.net/api/v1/password/?length=${user_input_text.value}&int=${user_input_number}`;
+request = (
+  user_input_text,
+  user_input_number,
+  user_input_upper,
+  user_input_special
+) => {
+  let url = `https://passwordwolf.com/api/?length=${user_input_text.value}&upper=${user_input_upper}&numbers=${user_input_number}&special=${user_input_special}&repeat=1`;
   fetch(url)
     .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      document.querySelector("#dispPassword").innerHTML = data.password;
+    .then((responseJson) => {
+      for (let { password } of responseJson) {
+        const apipass = document.createElement("p");
+        apipass.innerText = password;
+        document.querySelector("#dispPassword").innerHTML = password;
+        console.log(password);
+      }
     });
 };
