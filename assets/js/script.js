0723cb3b6ef7697cb17edceac0ec613899e8352f
Dominic-wells <joedomwells@gmail.com>
2022-09-17 13:01:18 +0100

    Added copy btn code to allow user to copy output

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,3 +45,11 @@ request = (
       }
     });
 };
+// Code assist from https://sam-ngu.medium.com/click-to-copy-to-clipboard-copy-and-paste-with-javascript-be7857de83ca
+const copyButton = document.getElementById("btn-copy");
+copyButton.addEventListener("click", (event) => {
+  // getting the text content that we want to copy
+  const content = document.getElementById("dispPassword").textContent;
+  // loading the content into our clipboard
+  navigator.clipboard.writeText(content);
+});
