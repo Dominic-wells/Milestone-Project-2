f126829448ba6a78899f6a950edd816c8a4bf8af
Dominic-wells <joedomwells@gmail.com>
2022-09-18 10:38:32 +0100

    Added password validate tool

--- assets/js/validatePass.js
+++ assets/js/validatePass.js
@@ -1,5 +1,12 @@
+//code guidance from https://iqcode.com/code/javascript/password-weakness-checker
+let timeout;
+const password = document.getElementById("PassDifficulty");
+const strengthBadge = document.getElementById("strengthDisp");
+const strongPassword = new RegExp(
+  "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})"
+);
 // most common passwords https://en.wikipedia.org/wiki/Wikipedia:10,000_most_common_passwords
-let usedList = [
+const usedList = [
   "123456",
   "password",
   "12345678",
@@ -101,4 +108,42 @@ let usedList = [
   "taylor",
   "matrix",
 ];
+//Chucknorris easter egg
 let chuckMode = new RegExp(/Chucknorris|chucknorris/g);
+//If else,else for password
+function analysePassword(PasswordParameter) {
+  if (usedList.includes(PasswordParameter)) {
+    strengthBadge.style.background = "orange";
+    strengthBadge.textContent = "One of the most common passwords..";
+    document.getElementById("passworddivbox").style.backgroundColor = "#1D1D1D";
+    console.log("onlist");
+  } else if (chuckMode.test(PasswordParameter)) {
+    strengthBadge.style.background = "orange";
+    document.getElementById("passworddivbox").style.backgroundColor = "gold";
+    strengthBadge.textContent =
+      "There is no chin behind Chuck Norris' beard. There is only another fist.";
+    console.log("The god Chuck");
+  } else if (strongPassword.test(PasswordParameter)) {
+    strengthBadge.style.backgroundColor = "green";
+    document.getElementById("passworddivbox").style.backgroundColor = "#1D1D1D";
+    strengthBadge.textContent = "Few passwords are safe this could be one ";
+    console.log("Strong");
+  } else {
+    strengthBadge.style.backgroundColor = "red";
+    document.getElementById("passworddivbox").style.backgroundColor = "#1D1D1D";
+    strengthBadge.textContent =
+      "Needs improvement, Go and read the password section again...";
+    console.log("Weak");
+  }
+}
+
+password.addEventListener("input", () => {
+  strengthBadge.style.display = "block";
+  clearTimeout(timeout);
+  timeout = setTimeout(() => analysePassword(password.value), 500);
+  if (password.value.length !== 0) {
+    strengthBadge.style.display != "block";
+  } else {
+    strengthBadge.style.display = "none";
+  }
+});
