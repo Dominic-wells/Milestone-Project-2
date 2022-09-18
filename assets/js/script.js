48c6bfdd6e85b26f94d5f34e40a83c383e8dd1bb
Dominic-wells <joedomwells@gmail.com>
2022-09-18 14:00:09 +0100

    Removed unused code

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,23 +6,3 @@ copyButton.addEventListener("click", (event) => {
   // loading the content into our clipboard
   navigator.clipboard.writeText(content);
 });
-
-// const sections = document.querySelectorAll("section");
-// const navLi = document.querySelectorAll("nav .container ul li");
-// window.onscroll = () => {
-//   var current = "";
-
-//   sections.forEach((section) => {
-//     const sectionTop = section.offsetTop;
-//     if (pageYOffset >= sectionTop - 60) {
-//       current = section.getAttribute("id");
-//     }
-//   });
-
-//   navLi.forEach((li) => {
-//     li.classList.remove("active");
-//     if (li.classList.contains(current)) {
-//       li.classList.add("active");
-//     }
-//   });
-// };
